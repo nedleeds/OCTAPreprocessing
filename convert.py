0c4bf5164632f23ef7fba0dbd4fde157138baa24
root <lll.dh@gmail.com>
2021-04-15 23:40:49 +0000

    add header

--- convert.py
+++ convert.py
@@ -1,7 +1,8 @@
-import os, shutil
-import SimpleITK as sitk
 import cv2
 import glob
+import os, shutil
+import SimpleITK as sitk
+import nibabel   as nib
 
 BMPDIR = '/root/Share/data' # BMPDIR is path for directory which has many directories that has a bunch of bmp images.
 NIIDIR = '/root/Share/nii'
@@ -12,12 +13,16 @@ class bmp2nii():
         self.i_OCTs = []
         self.datadir = DATADIR
         self.niidir = ''
+        self.niiname = ''
+        self.niipath = ''
         
     
-    def __call__(self, niidir):
+    def __call__(self, niidir, header=False):
         self.niidir = niidir
         if os.path.isdir(self.niidir): pass
         else: os.mkdir(self.niidir)
+        self.head = header
+
         for f in os.listdir(self.datadir):
             idx = f.split('.')[0] # OCT file has 5 legth number like 10001
             if len(idx)==5: # not rendered data.
@@ -45,8 +50,19 @@ class bmp2nii():
         reader = sitk.ImageSeriesReader()
         reader.SetFileNames(OCT_sorted)
         vol = reader.Execute()
-        niiname = f'{dnum}.nii.gz'
-        niipath = os.path.join(self.niidir, niiname)
-        sitk.WriteImage(vol, niipath)
+        self.niiname = f'{dnum}.nii.gz'
+        self.niipath = os.path.join(self.niidir, self.niiname)
+        sitk.WriteImage(vol, self.niipath)
+        #adding header if there's header file's path
+        if self.head:
+            self.addHeader()
 
-convert = bmp2nii(BMPDIR)(NIIDIR)
+    def addHeader(self):
+        '''
+        add header with .mhd file
+        '''
+        nib_img= nib.load(self.niipath)
+        print(nib_img.header)
+        input()
+
+convert = bmp2nii(BMPDIR)(NIIDIR, header=True)
