7f5663e0a40b1ace520f112b7fe802e49e5f6bd0
dhl <lllee.dh@gmail.com>
2021-05-08 11:30:31 +0000

    modify ( RGB to Gray )

--- convert.py
+++ convert.py
@@ -4,6 +4,7 @@ import os, shutil
 import SimpleITK as sitk
 import nibabel   as nib
 import numpy     as np
+from skimage.color import rgb2gray
 
 BMPDIR = '/root/Share/data/OCTA-500_6M_OCT' # BMPDIR is path for directory which has many directories that has a bunch of bmp images.
 NIIDIR = '/root/Share/data/nii'
@@ -25,22 +26,24 @@ class bmp2nii():
         self.head_ = header_
 
         for f in os.listdir(self.datadir):
-            idx = f.split('.')[0] # OCT file has 5 legth number like 10001
-            if len(idx)==5: # not rendered data.
+            idx = f.split('.')[0]           # OCT file has 5 legth number like 10001
+            if len(idx)==5:                 # not rendered data.
                 self.i_OCTs.append(int(idx))
-        self.i_OCTs = sorted(self.i_OCTs)
 
+        self.i_OCTs = sorted(self.i_OCTs)
         for i in self.i_OCTs:
             self.OCTs.append(os.path.join(self.datadir,str(i)))
 
         cnt = 0
         for OCT in self.OCTs:
-            # if cnt == 5: return
+            # if cnt == 1: return
             datanum=OCT.split('/')[-1]
             print(f'[{datanum}] : ',end='')
             self.bmp2nii(OCT,datanum)
             print('all the bmp images are converted to nii.')
             cnt +=1
+        
+  
 
     def bmp2nii(self, OCT, dnum):
         '''
@@ -50,15 +53,21 @@ class bmp2nii():
         reader = sitk.ImageSeriesReader()
         reader.SetFileNames(OCT_sorted)
         self.vol = reader.Execute()
+
+        image_array = sitk.GetArrayFromImage(self.vol)    #z, y, x
+        grayscale = rgb2gray(image_array)
+        g_image = sitk.GetImageFromArray(grayscale)
+        
         # self.convertAxis() 
         self.niiname = f'{dnum}.nii.gz'
         self.niipath = os.path.join(self.niidir, self.niiname)
-        sitk.WriteImage(self.vol, self.niipath)
+        sitk.WriteImage(g_image, self.niipath)
         
         # adding header if there's header file's path
         if self.head_ : self.addHeader(dnum)
         
-        nib_img= nib.load(self.nibpath)
+        # nib_img= nib.load(self.nibpath)
+        # print(nib_img.header)
 
 
     def convertAxis(self):
@@ -77,8 +86,9 @@ class bmp2nii():
         add header
         '''
         nib_img= nib.load(self.niipath)
+        # print(swi.header)
         head = nib_img.header
-        head['dim'][0:6]=[1,400,640,400,1,1]
+        head['dim'][0:4]=[3,400,640,400]
         head['pixdim'][1:4]=[15,3.125,15]
         head['xyzt_units']='3'
         head['qform_code']='0'
@@ -86,14 +96,15 @@ class bmp2nii():
         c = np.array(nib_img.get_fdata())
         nib_img2 = nib.Nifti1Image(c, nib_img.affine, header=head)
         head2 = nib_img2.header
-        head2['dim'][0:6]=[1,400,640,400,1,1] #<---------------Not working ...
+        head2['dim'][0:4]=[3,400,640,400] #<---------------Not working ...
         head2['pixdim'][1:4]=[15,3.125,15]
         head2['xyzt_units']='3'
 
-        self.nibname = dnum+'_nib.nii.gz'
+        self.nibname = dnum+'.nii.gz'
         self.nibpath = os.path.join(NIIDIR, self.nibname)
 
         nib.save(nib_img2, self.nibpath)
+        
 
         
         
