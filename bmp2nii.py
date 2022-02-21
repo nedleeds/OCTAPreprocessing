87a92d4358aebff36b2396558d00efe0c4e27f02
dhl <lllee.dh@gmail.com>
2022-02-21 18:52:10 +0900

    add modify nii header.

--- bmp2nii.py
+++ bmp2nii.py
@@ -1,16 +1,17 @@
 import os
 import glob
+import re
 import numpy     as np
 import nibabel   as nib
 import SimpleITK as sitk
 from skimage.color import rgb2gray
 
 class bmp2nifti():
-    def __init__(self):
+    def __init__(self, header_information):
         self.bmp_dir = ''
         self.nii_dir = ''
-        self.physical_resolution = {}
-        self.nii_size = {}
+        self.nii_size = []
+        self.info = header_information
 
     def set_dir(self, load_dir, save_dir):
         self.bmp_dir = load_dir
@@ -26,6 +27,7 @@ class bmp2nifti():
         for subject in subject_list:
             subject_bmp_dir = os.path.join(self.bmp_dir, subject)
             self.bmp2nii(idx=subject, load_dir=subject_bmp_dir, save_dir=self.nii_dir)
+            break
             
         
     def bmp2nii(self, idx, load_dir, save_dir):
@@ -39,21 +41,49 @@ class bmp2nifti():
         nifti = sitk.GetArrayFromImage(nifti_image)
         nifti = np.sum(nifti, axis=-1)/3 # RGB -> GRAY
         nifti = nifti.transpose(2,1,0)
-        
+
         # sitk image from array
         nifti_img = sitk.GetImageFromArray(nifti)
         nifti_img = sitk.Cast(sitk.RescaleIntensity(nifti_img), sitk.sitkUInt8)
 
         save_path = os.path.join(save_dir, f'{idx}.nii.gz')
         sitk.WriteImage(nifti_img, save_path)
+
+        self.modify_header(load_path=save_path)
         print(f"{idx} has been converted.")
 
-class modify_nifti_header():
+    def modify_header(self, load_path):
+        nifti_img = nib.load(load_path)
+        nifti_data = nifti_img.dataobj
+        nifti_info = nifti_img.header
+
+        w_resolution, h_resolution, d_resolution = self.info['resolution']
+        w_origin, h_origin, d_origin = self.info['origin']
+
+        nifti_info_new =nifti_info
+        nifti_info_new['xyzt_units'] = 0
+        nifti_info_new['pixdim'] = [1., w_resolution, h_resolution, d_resolution, 1., 1., 1., 1.]
+        nifti_info_new['qoffset_x'] = w_origin
+        nifti_info_new['qoffset_y'] = h_origin
+        nifti_info_new['qoffset_z'] = d_origin
+        nifti_info_new['srow_x'] = [w_resolution, 0, 0, w_origin]
+        nifti_info_new['srow_y'] = [0, h_resolution, 0, h_origin]
+        nifti_info_new['srow_z'] = [0, 0, d_resolution, d_origin]
+
+        nifti_new_img = nib.Nifti1Image(nifti_img.dataobj, nifti_info_new.get_best_affine())
+        nib.save(nifti_new_img, load_path)
+        
+        # sitk_img = sitk.ReadImage(load_path, sitk.sitkUInt8)
+        # sitk_img = sitk.Cast(sitk.RescaleIntensity(sitk_img), sitk.sitkUInt8)
+        # sitk.WriteImage(sitk_img, load_path)
+
+class modify_nifti_header(bmp2nifti):
     def __init__(self) -> None:
-        self.nii_size = {}
+        super.__init__()
         self.physical_resolution = {}
 
     def set_nifti_resolution(self, nii_size, physical_resolution):
+        assert nii_size == self.nii_size, 'your nifti size setting  is different from loaded.'
         height_nii, width_nii, depth_nii = nii_size
         height_res, width_res, depth_res = physical_resolution
         self.nii_size = {'height' : height_nii, 'width' : width_nii, 'depth' : depth_nii}
@@ -69,24 +99,29 @@ class modify_nifti_header():
 
 
 def main():
-    # This part is about converting bmp files to nifti.
-    bmp_dir = '/data/IEEE-OCT500/FOV_3MM/OCT'
-    nii_dir = '/data/Nifti/In/FOV_33/OCT'
-    converter = bmp2nifti()
-    converter.set_dir(load_dir = bmp_dir, save_dir = nii_dir)
-    converter.start_convert()
+    scale = 38*2
+    fov33_size = (3, 2, 3)  # fov66_size = (6, 6, 2)
+    nii33_size = (304, 604, 304)    # nii66_size = (400, 640, 400)
+    resolution = [(f/v)*scale for f,v in zip(fov33_size, nii33_size)]
+    origin = [-(r*v)//2 for r,v in zip(resolution, nii33_size)]
+    header_info = {'origin':origin, 'resolution':resolution}
 
-    # This part is about modifying header info for 
-    # proper 3D render view based on FOV.
-    scale = 32
-    fov33_size = (3*scale, 2*scale, 3*scale) 
-    nii33_size = (304, 640, 304)
-    # fov66_size = (6, 6, 2)
-    # nii66_size = (400, 640, 400)
-    modifier = modify_nifti_header()
-    modifier.set_nifti_resolution(nii_size = nii33_size,
-                                  physical_resolution = fov33_size)
-    modifier.check_volume_info()
+    # This part is about converting bmp files to nifti.
+    bmp_dir = ['/data/IEEE-OCT500/FOV_3MM/OCT', '/data/IEEE-OCT500/FOV_3MM/OCTA']
+    nii_dir = ['/data/Nifti/In/FOV_33/OCT','/data/Nifti/In/FOV_33/OCTA']
+    converter = bmp2nifti(header_information=header_info)
+    for b_dir, n_dir in zip(bmp_dir, nii_dir):
+        converter.set_dir(load_dir=b_dir, save_dir=n_dir)
+        converter.start_convert()
+
+    # # This part is about modifying header info for 
+    # # proper 3D render view based on FOV.
+    
+    
+    # modifier = modify_nifti_header(converter)
+    # modifier.set_nifti_resolution(nii_size = nii33_size,
+    #                               physical_resolution = output_resolution)
+    # modifier.check_volume_info()
     
 
 main()
